c3bc7ab55271640e444785d6f514fd916162ce35
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-01 16:33:03 +0200

    updated check error function to check for the name and comment of a champion

--- asm/error_gestion.c
+++ asm/error_gestion.c
@@ -10,13 +10,15 @@
 void print_arr(char **arr)
 {
     for (int i = 0; arr[i] != NULL; i++)
-        printf("%s\n", arr[i]);
+        printf("%i:%s$\n", i, arr[i]);
 }
 
 error_t *init_struct(void)
 {
     error_t *error = malloc(sizeof(*error));
 
+    error->have_name = 0;
+    error->have_comment = 0;
     return error;
 }
 
@@ -32,7 +34,9 @@ int check_error(char *argv[])
         return 84;
     while (getline(&line, &len, stream) != -1) {
         arr = str_to_arr(line);
-        print_arr(arr);
+        check_name_and_comment(error, arr);
     }
+    if (error->have_name != 1 || error->have_comment != 1)
+        return 84;
     return 0;
 }
