4de3bfd54ce976216342c380e4dccd997a4a128b
Kylian Labrador <kylian.labrador@epitech.eu>
2023-05-02 11:40:43 +0200

    [ADD] op & start verif function param

---
+++ asm/verif_functions_param.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** verif_functions_param
+*/
+
+#include "asm.h"
+
+int verif_functions_param(char *str)
+{
+    int i = 0;
+    int j = 0;
+    int count = 0;
+    char **arr = str_to_arr(str);
+
+    
+    return 0;
+}
