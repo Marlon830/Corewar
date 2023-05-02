e65a6e47feb3e36478254764f5c91650c8044923
Alexandre Bret <alexandre.bret@epitech.eu>
2023-05-02 11:42:56 +0200

    added struct list

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -10,12 +10,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct list_s {
+    char *label;
+    struct list_s *next;
+} list_t;
+
 typedef struct error_s {
     int have_name;
     int have_comment;
+    list_t *labels_defined;
+    list_t *labels_used;
 } error_t;
 
 char **str_to_arr(char *str);
 int my_strlen(char *str);
 int check_error(char *argv[]);
+void get_labels(char **arr, error_t *error);
 char **malloc_2d_array(int row, int col);
+
+list_t *init_list(void);
+void add_elem_to_list(list_t *list, char *label);
