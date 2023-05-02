3c09a1925f26f34bf8b5e73e2f383728f265f461
Alexandre Bret <alexandre.bret@epitech.eu>
2023-05-02 15:09:09 +0200

    added error gestion for labels

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct list_s {
     char *label;
@@ -24,12 +25,19 @@ typedef struct error_s {
 
 char **str_to_arr(char *str);
 int my_strlen(char *str);
+int is_alphanum(char c);
+char *my_strcpy(char *dest, char *src);
 int check_error(char *argv[]);
 void get_labels(char **arr, error_t *error);
+int is_name_labels_correct(list_t *list);
+int is_same_label(list_t *list);
+int check_label_exist(list_t *label_defined, list_t *label_used);
 char **malloc_2d_array(int row, int col);
 
 list_t *init_list(void);
 void add_elem_to_list(list_t *list, char *label);
+void print_list(list_t *list);
+
 int my_strcmp(char *s1, char *s2);
 int how_many_char_in_str(char c, char *str);
 void check_name_and_comment(error_t *error, char **arr);
