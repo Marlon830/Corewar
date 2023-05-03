d0ec158fad075dda339e5796bdf125ced97b49c0
allan.leherpeux <allan.leherpeux@epitech.eu>
2023-05-03 10:19:31 +0200

    [UPDATE] change flags handler

--- corewar/flags.c
+++ corewar/flags.c
@@ -27,6 +27,8 @@ int check_dump(char **argv, int id, vm_t *vm)
 
 int check_next_address(char **argv, int id, vm_t *vm)
 {
+    if (argv[id] == NULL)
+        return 84;
     if (!my_strcmp(argv[id], "-a") && is_arg(argv[id + 1])) {
         if (vm->load_address != -1)
             return 84;
@@ -38,6 +40,8 @@ int check_next_address(char **argv, int id, vm_t *vm)
 
 int check_next_prog_num(char **argv, int id, vm_t *vm)
 {
+    if (argv[id] == NULL)
+        return 84;
     if (!my_strcmp(argv[id], "-n") && is_arg(argv[id + 1])) {
         if (vm->prog_number != -1)
             return 84;
@@ -50,17 +54,12 @@ int check_next_prog_num(char **argv, int id, vm_t *vm)
 int check_args(char **argv, int id, vm_t *vm)
 {
     int flag_return = 0;
+    int i = 0;
 
+    vm->load_address = -1;
+    vm->prog_number = -1;
     flag_return = check_dump(argv, id, vm);
     if (flag_return != 0)
         return flag_return;
-    flag_return = check_next_address(argv, id, vm);
-    if (flag_return != 0)
-        return flag_return;
-    flag_return = check_next_prog_num(argv, id, vm);
-    if (flag_return != 0)
-        return flag_return;
-    if (check_champions_exist(argv[id], vm))
-        return 0;
-    return 84;
+    return check_valid_champ(argv, id, vm);
 }
