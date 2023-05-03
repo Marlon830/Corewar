72fe0420e9665a571abe05d36b2caabb60ee3b5b
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-03 09:54:13 +0200

    added error gestion for the strcmp function

--- asm/strings_bis.c
+++ asm/strings_bis.c
@@ -22,6 +22,8 @@ int my_strcmp(char *s1, char *s2)
 {
     int i = 0;
 
+    if (s1 == NULL || s2 == NULL)
+        return -1;
     while (s1[i] == s2[i]) {
         i = i + 1;
         if (s1[i] == '\0' && s2[i] == '\0') {
