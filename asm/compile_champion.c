524d38163c7d9c4ee98d7d80c4447716ddc03dca
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-04 12:59:41 +0200

    fixed values that will be written on the binary and added a function to write the header struct
    Co-authored-by: 1NainConnu34 <1NainConnu34@users.noreply.github.com>

--- asm/compile_champion.c
+++ asm/compile_champion.c
@@ -22,24 +22,34 @@ char *get_output_file(char *argv[])
     return ans;
 }
 
-void compile_champion(char *argv[])
+void write_header(compil_t *compil, char *argv[], int output_fd)
+{
+    header_t header = {0};
+
+    header.magic = 0xea83f3;
+    header.prog_size = compil->nb_bytes;
+    my_strcpy(header.prog_name, get_header_value(argv, ".name"));
+    my_strcpy(header.comment, get_header_value(argv, ".comment"));
+    write(output_fd, &header, sizeof(header));
+}
+
+void compile_champion(compil_t *compil, char *argv[])
 {
     FILE *stream = fopen(argv[1], "r");
-    int output_fd = open(get_output_file(argv), O_CREAT | O_TRUNC, 0644);
+    int output_fd = open(get_output_file(argv),
+    O_RDWR | O_CREAT | O_TRUNC, 0644);
     char *line = NULL;
     size_t len = 0;
     char **arr;
-    header_t header = {.magic = 0xea83f3,
-    .prog_size = get_prog_size(argv)};
-    my_strcpy(header.prog_name, get_header_value(argv, ".name"));
-    my_strcpy(header.comment, get_header_value(argv, ".comment"));
 
-    write(output_fd, &header, sizeof(header));
+    write_header(compil, argv, output_fd);
     while (getline(&line, &len, stream) != -1) {
         arr = str_to_arr(line);
         if (arr[0] == NULL)
             continue;
-        write_instructions(output_fd, arr);
+        write_instructions(output_fd, arr, compil);
     }
+    fclose(stream);
+    close(output_fd);
     return;
 }
