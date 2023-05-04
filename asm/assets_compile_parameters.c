a8e722dcb0baae623d23aee53f0b9aacee1101aa
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-04 22:05:17 +0200

    fixed get_nb_to_write to compute the exact offset of the parameter

--- asm/assets_compile_parameters.c
+++ asm/assets_compile_parameters.c
@@ -7,12 +7,17 @@
 
 #include "asm.h"
 
-int get_nb_to_write(char *param, compil_t *compil)
+int get_nb_to_write(char *param, compil_t *compil, int is_direct)
 {
     int nb = 0;
-    label_t *label = get_label_with_name(compil->label_list,
-    param + (param[0] == ':' ? 1 : 2));
+    label_t *label;
+    int offset = 0;
 
+    if (is_direct)
+        offset += 1;
+    if (param[0 + offset] == ':')
+        offset += 1;
+    label = get_label_with_name(compil->label_list, param + offset);
     if (label != NULL)
         nb = (label->pos + compil->bytes_line_pos - 1) - compil->act_pos;
     else
