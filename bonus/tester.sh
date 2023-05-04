3c5b6ac639cec11f44ad5b4ad5dd8db3ddfb6ffd
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-04 20:09:21 +0200

    updated tester, now it displays the error

--- bonus/tester.sh
+++ bonus/tester.sh
@@ -9,6 +9,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+BOLD='\033[1m'
 
 for fich in `ls ../champions/$fich`
 do
@@ -18,10 +19,15 @@ do
     new_fich=$fich_no_ext.cor
     if cmp -s ../champions/$new_fich $new_fich
     then
-        echo -e "$fich : ${GREEN}SUCCESS${NC}"
+        echo -e "$fich : ${GREEN}${BOLD}SUCCESS${NC}"
     else
-        echo -e "$fich : ${RED}FAILURE${NC}"
+        echo -e "$fich : ${RED}${BOLD}FAILURE${NC}"
+        hexdump -C ../champions/$new_fich > ../champions/$new_fich.hex
+        hexdump -C $new_fich > $new_fich.hex
+        diff ../champions/$new_fich.hex $new_fich.hex
     fi
 done
 rm ../champions/*.cor
 rm *.cor
+rm ../champions/*.hex
+rm *.hex
