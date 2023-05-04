39ac244e142d7d4ae645a87e0fa6a1175f534276
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-04 13:01:35 +0200

    added function to write the indexes and added a int to count where is the compilation to know the values for the label indexes

--- asm/compile_parameters.c
+++ asm/compile_parameters.c
@@ -7,15 +7,16 @@
 
 #include "asm.h"
 
-void write_reg(int output_fd, char *param)
+void write_reg(int output_fd, char *param, compil_t *compil)
 {
     char reg = 0;
 
     reg = my_getnbr(param);
     write(output_fd, &reg, 1);
+    compil->act_pos += 1;
 }
 
-void write_ind(int output_fd, char *param)
+void write_ind(int output_fd, char *param, compil_t *compil)
 {
     char byte1 = 0;
     char byte2 = 0;
@@ -27,9 +28,10 @@ void write_ind(int output_fd, char *param)
     set_bit_at(&byte2, 1, (nb >> 3 & 1));
     write(output_fd, &byte2, 1);
     write(output_fd, &byte1, 1);
+    compil->act_pos += 2;
 }
 
-void write_dir(int output_fd, char *param)
+void write_dir(int output_fd, char *param, compil_t *compil)
 {
     char byte1 = 0;
     char byte2 = 0;
@@ -49,9 +51,32 @@ void write_dir(int output_fd, char *param)
     write(output_fd, &byte3, 1);
     write(output_fd, &byte2, 1);
     write(output_fd, &byte1, 1);
+    compil->act_pos += 4;
 }
 
-void write_parameters(int output_fd, char **arr)
+void write_index(int output_fd, char *param, compil_t *compil)
+{
+    label_t *label;
+    char byte1 = 0;
+    char byte2 = 0;
+    int nb = 0;
+
+    if (param[1] != ':') {
+        write_ind(output_fd, param, compil);
+        return;
+    }
+    label = get_label_with_name(compil->label_list, param + 2);
+    nb = label->pos - compil->act_pos;
+    set_bit_at(&byte1, 0, (nb >> 0 & 1));
+    set_bit_at(&byte1, 1, (nb >> 1 & 1));
+    set_bit_at(&byte2, 0, (nb >> 2 & 1));
+    set_bit_at(&byte2, 1, (nb >> 3 & 1));
+    write(output_fd, &byte2, 1);
+    write(output_fd, &byte1, 1);
+    compil->act_pos += 2;
+}
+
+void write_parameters(int output_fd, char **arr, compil_t *compil)
 {
     int pos = 0;
 
@@ -60,17 +85,15 @@ void write_parameters(int output_fd, char **arr)
     else
         pos += 2;
     for (; arr[pos] != NULL; pos++) {
-        if (is_reg(arr[pos])) {
-            write_reg(output_fd, arr[pos]);
+        if (is_index(arr, pos)) {
+            write_index(output_fd, arr[pos], compil);
             continue;
         }
-        if (is_dir(arr[pos])) {
-            write_dir(output_fd, arr[pos]);
-            continue;
-        }
-        if (is_ind(arr[pos])) {
-            write_ind(output_fd, arr[pos]);
-            continue;
-        }
+        if (is_reg(arr[pos]))
+            write_reg(output_fd, arr[pos], compil);
+        if (is_dir(arr[pos]))
+            write_dir(output_fd, arr[pos], compil);
+        if (is_ind(arr[pos]))
+            write_ind(output_fd, arr[pos], compil);
     }
 }
