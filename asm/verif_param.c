d6e447886e6945ac7f76832c82cc4ad2e068b2f4
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-04 13:09:09 +0200

    added function to know if a param is an index
    Co-authored-by: 1NainConnu34 <1NainConnu34@users.noreply.github.com>

--- asm/verif_param.c
+++ asm/verif_param.c
@@ -7,13 +7,46 @@
 
 #include "asm.h"
 
-int is_alphanumeric(char c)
+int is_index_bis(char **arr, int pos, char *instruction)
 {
-    if ((c >= 'a' && c <= 'z') ||
-    (c >= 'A' && c <= 'Z') ||
-    (c >= '0' && c <= '9') || c == '-')
+    if (!my_strcmp(instruction, "sti")) {
+        if (!my_strcmp(instruction, arr[0]) &&
+        ((pos == 2 && !is_reg(arr[2])) || (pos == 3 && !is_reg(arr[3]))))
+            return 1;
+        if (!my_strcmp(instruction, arr[1]) &&
+        ((pos == 3 && !is_reg(arr[3])) || (pos == 4 && !is_reg(arr[4]))))
+            return 1;
+    }
+    if (!my_strcmp(instruction, "fork"))
         return 1;
+    if (!my_strcmp(instruction, "lldi")) {
+        if (!my_strcmp(instruction, arr[0]) &&
+        (pos == 1 || pos == 2))
+            return 1;
+        if (!my_strcmp(instruction, arr[1]) &&
+        (pos == 2 || pos == 3))
+            return 1;
+    }
     return 0;
+}
+
+int is_index(char **arr, int pos)
+{
+    char *instruction = get_instruction(arr);
+
+    if (!my_strcmp(instruction, "zjmp"))
+        return 1;
+    if (!my_strcmp(instruction, "ldi")) {
+        if (!my_strcmp(instruction, arr[0]) &&
+        (pos == 1 || pos == 2))
+            return 1;
+        if (!my_strcmp(instruction, arr[1]) &&
+        (pos == 2 || pos == 3))
+            return 1;
+    }
+    if (!my_strcmp(instruction, "lfork"))
+        return 1;
+    return is_index_bis(arr, pos, instruction);
 }
 
 int is_reg(char *str)
