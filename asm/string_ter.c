1636c668c3df81cb2ea4e2fe42b1484b93edc59e
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-04 14:52:02 +0200

    fixed my_getnbr
    Co-authored-by: 1NainConnu34 <1NainConnu34@users.noreply.github.com>

--- asm/string_ter.c
+++ asm/string_ter.c
@@ -7,46 +7,28 @@
 
 #include "asm.h"
 
-int check_str(char const *str)
+int is_num(char c)
 {
-    int i = 0;
-    int len = 0;
-
-    while (str[i] < '0' || str[i] > '9') {
-        if (str[i] != '-' && str[i] != '+')
-            return 84;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9') {
-        len++;
-        i++;
-    }
-    if (len > 10)
-        return 84;
-    return 0;
+    return (c >= '0' && c <= '9');
 }
 
-int my_getnbr(char const *str)
+int my_getnbr(char *str)
 {
-    int signe = 0;
+    int number = 0;
+    int sign = 1;
     int i = 0;
-    int nbr = 0;
 
-    while (str[i] < '0' || str[i] > '9') {
-        if (str[i] == '-')
-            signe = signe + 1;
+    if (str[i] == '-') {
+        sign = -1;
         i++;
     }
-    while (str[i] >= '0' && str[i] <= '9') {
-        if (nbr == 214748364 && str[i] == '8' && signe % 2 == 1
-        && (str[i + 1] < '0' || str[i + 1] > '9'))
-            return -2147483648;
-        nbr = 10 * nbr + str[i] - 48;
+    while (!is_num(str[i]) && str[i] != '\0')
         i++;
-        if (nbr < 0 || check_str(str) == 84)
-            return 0;
+    while (is_num(str[i])) {
+        number = number * 10 + (str[i] - '0');
+        i++;
     }
-    return signe % 2 == 1 ? nbr * -1 : nbr;
+    return sign * number;
 }
 
 void my_putchar(char c)
