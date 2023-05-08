694f6f21b00b41e9ddd5ce99b1161cc5c04fb3a1
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-08 23:10:30 +0200

    added the start of the criterion tests for the asm

---
+++ tests/tests_asm/tests_asm.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** Corewar
+** File description:
+** tests_asm.c
+*/
+
+#include "asm.h"
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(test, test_for_test) {
+    compil_t *compil = init_compil_struct();
+    char **argv = malloc(sizeof(char *) * 3);
+
+    for (int i = 0; i < 3; i++)
+        argv[i] = malloc(sizeof(char) * 17);
+    argv[0] = "./asm";
+    argv[1] = "../champions/zork.s";
+    argv[2] = NULL;
+    get_all_labels_info(compil, argv);
+    compile_champion(compil, argv);
+}
