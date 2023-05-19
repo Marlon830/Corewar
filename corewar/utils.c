c7b2550f09bd94f1012f0649f8020e2108310fab
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-19 18:17:32 +0200

    added function to manipulate bits (set bit at and get bit at)
    Co-authored-by: 1NainConnu34 <1NainConnu34@users.noreply.github.com>

--- corewar/utils.c
+++ corewar/utils.c
@@ -21,3 +21,18 @@ void push(list_t **list, void *data)
     for (; tmp->next != NULL; tmp = tmp->next);
     tmp->next = node;
 }
+
+int get_bit_at(int x, int n)
+{
+    return ((x >> n) & 1);
+}
+
+void set_bit_at(char *x, int n, int value)
+{
+    *x = *x | value << n;
+}
+
+void set_bit_int_at(int *x, int n, int value)
+{
+    *x = *x | value << n;
+}
