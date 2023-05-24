8f2b6a0ef7da94d4b41f9674f82c994a78fe95ef
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-24 11:27:31 +0200

    updated fork instruction to add copy champion call function
    Co-authored-by: 1NainConnu34 <1NainConnu34@users.noreply.github.com>

--- corewar/all_instructions/instructions_ter.c
+++ corewar/all_instructions/instructions_ter.c
@@ -82,11 +82,11 @@ void exec_sti(vm_t *vm, champion_t *champion)
 void exec_fork(vm_t *vm, champion_t *champion)
 {
     int byte_size = 3;
-    champion_t new_champ = *champion;
+    champion_t *new_champ = copy_champion(champion);
     int act_pc = champion->pc + 1;
     int param = analyze_type(3, &act_pc, champion, vm);
 
-    new_champ.pc = champion->pc + param % IDX_MOD;
-    push(&vm->champ_list, &new_champ);
+    new_champ->pc = champion->pc + param % IDX_MOD;
+    push(&vm->champ_list, new_champ);
     champion->pc += byte_size;
 }
