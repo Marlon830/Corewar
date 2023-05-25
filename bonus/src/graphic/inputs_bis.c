2d925949a1bd78e978b4caa135b415505e6389a0
allan.leherpeux <allan.leherpeux@epitech.eu>
2023-05-25 15:37:34 +0200

    [ADD] inputs_bis

---
+++ bonus/src/graphic/inputs_bis.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** GraphicCorewar
+** File description:
+** inputs_bis.c
+*/
+
+#include "corewar.h"
+
+void socket_input(app_t *app)
+{
+    update_buttons(app, app->socket->buttons);
+}
+
+void corewar_input(app_t *app)
+{
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+        PlaySound(app->menu->fxBoom);
+}
