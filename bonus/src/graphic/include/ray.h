6443e9ab3321028650949ac7b3dfb8b7f3bc38fb
allan.leherpeux <allan.leherpeux@epitech.eu>
2023-05-25 15:39:07 +0200

    [ADD] ray.h

---
+++ bonus/src/graphic/include/ray.h
@@ -0,0 +1,59 @@
+/*
+** EPITECH PROJECT, 2022
+** GraphicCorewar
+** File description:
+** ray.h
+*/
+
+#pragma once
+
+#include "raylib.h"
+#include "rlgl.h"
+#include <stddef.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define RAYLIB_NEW_RLGL
+#define LETTER_BOUNDRY_SIZE     0.25f
+#define TEXT_MAX_LAYERS         32
+#define LETTER_BOUNDRY_COLOR    VIOLET
+
+typedef struct param_s {
+    int codepoint;
+    float textOffsetX;
+    float textOffsetY;
+    int index;
+    float scale;
+} param_t;
+
+typedef struct char_info_s {
+    Vector3 position;
+    float x;
+    float y;
+    float z;
+    float tx;
+    float ty;
+    float tw;
+    float th;
+    float width;
+    float height;
+} char_info_t;
+
+typedef struct d_text_s {
+        Vector3 position;
+        Color tint;
+        float fontSize;
+        float fontSpacing;
+        float lineSpacing;
+        bool backface;
+} d_text_t;
+
+typedef struct color_mode_s {
+    Color cube_color;
+    Color wire_color;
+    Color text_color;
+    Color background;
+} color_mode_t;
+
+void drawtext3d(d_text_t *param, const char *text);
+char_info_t *init_char_info(Rectangle srcRec, Font font, Vector3 position);
