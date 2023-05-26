ba9480be3547e634cbf47203c42fc1754f436beb
allan.leherpeux <allan.leherpeux@epitech.eu>
2023-05-26 10:22:27 +0200

    [UPDATE] client & server packet

--- bonus/src/graphic/include/socket.h
+++ bonus/src/graphic/include/socket.h
@@ -16,8 +16,18 @@
 
 #pragma once
 
-typedef struct packet_s {
+typedef enum packet_type {
+    CYCLE = 0,
+    OTHER
+} packet_type;
+
+typedef struct server_s {
     char *arena;
     char *champ_bytes;
     int *pc;
-} packet_t;
+} server_t;
+
+typedef struct client_s {
+    enum packet_type type;
+    int value;
+} client_t;
