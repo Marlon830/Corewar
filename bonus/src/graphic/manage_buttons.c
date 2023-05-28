067295de382cbd1b5f77a9fbeade9619bc8bb844
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-05-28 12:00:31 +0200

    added all function to manage buttons
    Co-authored-by: 1NainConnu34 <1NainConnu34@users.noreply.github.com>

---
+++ bonus/src/graphic/manage_buttons.c
@@ -0,0 +1,74 @@
+/*
+** EPITECH PROJECT, 2022
+** bonus
+** File description:
+** manage_buttons.c
+*/
+
+#include "corewar.h"
+
+void play_corewar(app_t *app)
+{
+    app->corewar->is_playing = true;
+    app->corewar->is_stopped = false;
+    app->corewar->is_cycling = false;
+    app->corewar->cycle_speed = app->corewar->save_speed;
+}
+
+void pause_corewar(app_t *app)
+{
+    app->corewar->is_playing = false;
+    app->corewar->is_cycling = false;
+    app->corewar->is_stopped = false;
+    app->corewar->save_speed = app->corewar->cycle_speed;
+}
+
+void stop_corewar(app_t *app)
+{
+    app->corewar->is_playing = true;
+    app->corewar->is_stopped = true;
+    app->corewar->is_cycling = false;
+    app->corewar->cycle_speed = 1;
+}
+
+void cycle_l(app_t *app)
+{
+    app->corewar->cycle_speed = -1;
+    app->corewar->is_cycling = true;
+    app->corewar->is_playing = true;
+    app->corewar->is_stopped = false;
+}
+
+void cycle_r(app_t *app)
+{
+    app->corewar->cycle_speed = 1;
+    app->corewar->is_cycling = true;
+    app->corewar->is_playing = true;
+    app->corewar->is_stopped = false;
+}
+
+void speed_l(app_t *app)
+{
+    if (app->corewar->cycle_speed > -64 && app->corewar->cycle_speed < 0)
+        app->corewar->cycle_speed += app->corewar->cycle_speed;
+    if (app->corewar->cycle_speed == 1)
+        app->corewar->cycle_speed = -1;
+    if (app->corewar->cycle_speed > 1)
+        app->corewar->cycle_speed -= app->corewar->cycle_speed / 2;
+    app->corewar->is_playing = true;
+    app->corewar->is_cycling = false;
+    app->corewar->is_stopped = false;
+}
+
+void speed_r(app_t *app)
+{
+    if (app->corewar->cycle_speed < 64 && app->corewar->cycle_speed > 0)
+        app->corewar->cycle_speed += app->corewar->cycle_speed;
+    if (app->corewar->cycle_speed == -1)
+        app->corewar->cycle_speed = 1;
+    if (app->corewar->cycle_speed < -1)
+        app->corewar->cycle_speed -= app->corewar->cycle_speed / 2;
+    app->corewar->is_playing = true;
+    app->corewar->is_cycling = false;
+    app->corewar->is_stopped = false;
+}
