dcbbfa7b49cb5d59f861c319993f293e0d915928
Kylian Labrador <kylian.labrador@epitech.eu>
2023-05-28 13:49:29 +0200

    hover and clicked button

--- bonus/src/graphic/init_corewar_buttons.c
+++ bonus/src/graphic/init_corewar_buttons.c
@@ -13,37 +13,37 @@ void init_corewar_buttons(app_t *app, corewar_t *corewar)
 
     tmp = create_button("src/graphic/assets/play_corewar.png",
     (Vector2){app->screenWidth  - 600, 0},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &play_corewar;
     push(&corewar->buttons, tmp);
     tmp = create_button("src/graphic/assets/pause_corewar.png",
     (Vector2){app->screenWidth  - 900, 0},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &pause_corewar;
     push(&corewar->buttons, tmp);
     tmp = create_button("src/graphic/assets/stop_corewar.png",
     (Vector2){app->screenWidth - 1200, 0},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &stop_corewar;
     push(&corewar->buttons, tmp);
     tmp = create_button("src/graphic/assets/cycle_l.png",
     (Vector2){app->screenWidth - 900, 300},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &cycle_l;
     push(&corewar->buttons, tmp);
     tmp = create_button("src/graphic/assets/cycle_r.png",
     (Vector2){app->screenWidth - 300, 300},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &cycle_r;
     push(&corewar->buttons, tmp);
     tmp = create_button("src/graphic/assets/speed_l.png",
     (Vector2){app->screenWidth - 1500, 0},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &speed_l;
     push(&corewar->buttons, tmp);
     tmp = create_button("src/graphic/assets/speed_r.png",
     (Vector2){app->screenWidth  - 300, 0},
-    (Vector2){300, 300}, 1);
+    (Vector2){75, 75}, 3);
     tmp->clicked = &speed_r;
     push(&corewar->buttons, tmp);
 }
